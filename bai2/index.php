3ac501e0df745fe3f01b88fec5910926e8a7ef24
dangngocthanh <helomilo123@gmail.com>
2021-10-21 08:16:05 +0700

    a

---
+++ bai2/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        light,sup{
+            color : blue;
+        }
+    </style>
+</head>
+<body>
+<h1>Việt Nam</h1>
+<hr>
+<p style="text-indent: 50px "><strong>Việt Nam</strong> (tên chính thức: <strong>Cộng hòa Xã hội chủ nghĩa Việt Nam</strong>)là một quốc gia nằm ở phía đông, ở giữa Lào và Campuchia, thuộc <light style="color: aqua">bán đảo Đông Dương</light>, thuộc khu vực <light>Đông Nam Á </light>thuộc châu Á</p>
+<p>Phía bắc Việt Nam giáp với <light>Trung Quốc</light>, phía Tây Việt Nam giáp với <light>Lào</light> và <light>Campuchia </light>, phía tây nam giáp <light>vịnh Thái Lan</light>, phía đông và phía nam giáp <light>biển Đông</light> và có hơn 4.000 hòn đảo, <sup>[6]</sup> bãi đá ngầm lớn nhỏ, gần và xa bờ, có vùng nội thủy, lãnh hải, vùng đặc quyền kinh tế và thềm lục địa được <light>Chính phủ Việt Nam </light>công nhận gấp ba lần diện tích đất liền (khoảng trân 1 tiệu km<sup style="color=black">2</sup>). Trên biển Đông có quần đảo <light>Trường Sa </light>và <light>Hoàng Sa</light> được Việt Nam tuyên bố chỉ quyền nhưng đang trong vòng tranh chấp với các quốc gia <light>Đài Loan</light>, <light>Trung Quốc</light>, <light>Malaysia</light> và <light>Philippines</light>.</p>
+</body>
+</html>
